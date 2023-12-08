2c7e96723f152a933dc5c7ed48e8b8da353ad73a
Keith Capers <K.Capers94@gmail.com>
2023-12-08 12:28:53 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    let newTask = document.querySelector('#new-task-description')
+    newToDos(newTask.value)
+
+  }
+  )});
+
+  function newToDos(task) {
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent ='x'
+    li.textContent = `${task}`
+    li.appendChild(btn)
+    document.querySelector('#tasks').appendChild(li)
+  }
+  
+  function handleDelete (e) {
+    e.target.parentNode.remove()
+  }
+
+
